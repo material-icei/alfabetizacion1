730b1ab9c814bbca9337c1fc177dcfe59645f1f9
Material ICEI <material@colegioicei.edu.ar>
2026-08-18 14:04:45 -0300

    Add new color legends for various items

--- colorear/data.js
+++ colorear/data.js
@@ -41,5 +41,55 @@ const PRESET_ACTIVITIES = [
       { letter: "G", color: "#7F7F7F" },
       { letter: "H", color: "#22B14C" }
     ]
+  },
+  {
+    id: "tren",
+    name: "EL TREN",
+    image: "images/tren.jpg",
+    legend: [
+      { letter: "A", color: "#FEF200" },
+      { letter: "B", color: "#FF8027" },
+      { letter: "C", color: "#23B14D" },
+      { letter: "D", color: "#ED1C24" },
+      { letter: "E", color: "#3F47CC" },
+      { letter: "F", color: "#A349A3" },
+      { letter: "G", color: "#B97A57" },
+      { letter: "H", color: "#FFFFFF" }
+    ]
+  },
+  {
+    id: "lorito",
+    name: "EL LORITO",
+    image: "images/lorito.jpg",
+    legend: [
+      { letter: "ta", color: "#FF9700" },
+      { letter: "te", color: "#4EB051" },
+      { letter: "ti", color: "#424242" },
+      { letter: "to", color: "#FFFFFF" },
+      { letter: "tu", color: "#64B5F6" }
+    ]
+  },
+  {
+    id: "dino",
+    name: "EL DINOSAURIO",
+    image: "images/dino3.jpg",
+    legend: [
+      { letter: "sa", color: "#FFFFFF" },
+      { letter: "se", color: "#DCDCDC" },
+      { letter: "si", color: "#B8B8BA" },
+      { letter: "so", color: "#8F8F8F" },
+      { letter: "su", color: "#767676" }
+    ]
+  },
+  {
+    id: "ave",
+    name: "EL AVE",
+    image: "images/ave.jpg",
+    legend: [
+      { letter: "B", color: "#0000FE" },
+      { letter: "R", color: "#FE0100" },
+      { letter: "I", color: "#FF9932" },
+      { letter: "D", color: "#FFFE01" }
+    ]
   }
 ];
